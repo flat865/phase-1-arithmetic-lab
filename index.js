fd4fe4fc5fe51a7c8f158654cc388dc682658d4e
Alfonso Lopez Cruz <alfonso2081lopez@gmail.com>
2022-01-08 22:43:18 -0500

    commit tree 09080cc22aa59b5eac2abf6254b7fd92ec6cfe69

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+let num1 = 31
+let num2 = 2
+const multiply = 31 * 2;
+let random = Math.floor(Math.random() * 10) + 1;
+let num3 = 24
+let num4 = 6
+const mod = 24 / 6;
+let max = Math.max(1, 2, 3, 4, 20);
